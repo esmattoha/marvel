8d12070fd0be4edda854022307b72b4778a7193c
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-03-23 13:39:09 -0700

    multer fixing

--- routers/movie.js
+++ routers/movie.js
@@ -1,9 +1,43 @@
 // Dependencies
 const express = require('express');
-const movieController = require('../controllers/movie');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../upload/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().toISOString() + file.originalname);
+    }
+  });
+
+const upload = multer({ storage: storage});
+
+// const movieController = require('../controllers/movie');
+const Movie = require('../models/movie');
 
 const router = express.Router();
 
-router.post('/movie', movieController.post_movie);
+router.post('/movie', upload.single('imageUrl'), (req, res)=>{
+    console.log(req.file);
+    const movie = new Movie({
+        title: req.body.title,
+        description: req.body.description,
+        releaseDate: req.body.releaseDate,
+        genre: req.body.genre,
+        timeDuration: req.body.timeDuration,
+        point: req.body.point,
+      });
+      movie
+        .save()
+        .then((result) => {
+          res.status(201).send(result);
+        })
+        .catch((err) => {
+          res.status(422).json({
+            message: "Something went wrong , please try some time later! ",
+          });
+        });
+});
 
 module.exports = router;
